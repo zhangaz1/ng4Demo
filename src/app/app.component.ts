0803170e24ac1c37db6e15f1cd256292af25f6c4
zhangaz1 <zhangaz1@hotmail.com>
2017-02-22 16:41:47 +0800

    实现主从联动

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,10 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm',
-  };
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
