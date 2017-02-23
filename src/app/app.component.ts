be11fccff61ed6f7e21568c19bc44a57c8a03b22
zhangaz1 <zhangaz1@hotmail.com>
2017-02-23 21:30:28 +0800

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
