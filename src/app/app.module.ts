064608c49ff0561dd4db846789d351ce16a6489e
zhangaz1 <zhangaz1@hotmail.com>
2017-02-23 10:29:08 +0800

    引入RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { HeroService } from './hero.service';
 
@@ -19,6 +20,12 @@ import { HeroDetailComponent } from './hero-detail.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      }
+    ]),
   ],
   providers: [
     HeroService,
