8da541411d080a0030aa490b32ac9b2975aaba52
zhangaz1 <zhangaz1@hotmail.com>
2017-02-24 17:53:51 +0800

    add in memory db service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule }  from '@angular/platform-browser';
 import { NgModule }       from '@angular/core';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+import { InMemoryWebApiModule }   from 'angular-in-memory-web-api';
+import { InMemoryDataService }    from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -21,6 +25,8 @@ import { HeroDetailComponent }  from './hero-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule,
   ],
   providers: [
