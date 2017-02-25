f75aa2feb1a2b1a34fbad63b1458a9eb89ffbd34
zhangaz1 <zhangaz1@hotmail.com>
2017-02-25 17:08:37 +0800

    Component 完成

--- src/app/hero-form.component.ts
+++ src/app/hero-form.component.ts
@@ -9,4 +9,27 @@ import { Hero }     from './hero';
 })
 export class HeroFormComponent {
 
+    powers = [
+        'Really Smart',
+        'Super Flexible',
+        'Super Hot',
+        'Weather Changer',
+    ];
+
+    model = new Hero(
+                    18, 
+                    'Dr IQ', 
+                    this.powers[0], 
+                    'Chuck Overstreet'
+                );
+
+    submitted = false;
+
+    onSubmit() {
+        this.submitted = true;
+    }
+
+    get diagnostic() {
+        return JSON.stringify(this.model);
+    }
 }
