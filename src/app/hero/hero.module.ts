112452fe4e0a555c280d6ee28fe2b1c29db8e3b2
zhangaz1 <zhangaz1@hotmail.com>
2017-02-26 18:07:35 +0800

    使用shared、not core

--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -1,6 +1,5 @@
 import { NgModule }     from '@angular/core';
 
-import { CoreModule }       from './../core/core.module';
 import { SharedModule }     from './../shared/shared.module';
 
 import { InMemoryWebApiModule }   from 'angular-in-memory-web-api';
@@ -15,7 +14,6 @@ import { HeroFormComponent }    from './form/hero-form.component';
 
 @NgModule({
     imports: [
-        // CoreModule,
         SharedModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService),
     ],
