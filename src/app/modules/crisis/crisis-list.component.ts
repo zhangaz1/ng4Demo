e24627d66dfaf02d2c134f7925754862dc9d47f4
zhangaz1 <zhangaz1@hotmail.com>
2017-02-28 18:25:17 +0800

    关键帧

--- src/app/modules/crisis/crisis-list.component.ts
+++ src/app/modules/crisis/crisis-list.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit,
           style,
           transition,
           animate,
+          keyframes,
          } from '@angular/core';
 
 import { Crisis }            from './../../models/crisis';
@@ -14,19 +15,20 @@ import { CrisisService }     from './crisis.service';
     templateUrl: './crisis-list.component.html',
     animations: [
         trigger('crisisState', [
-            state('in', style({opacity: 1, transform: 'translateX(0)'})),
+            state('in', style({transform: 'translateX(0)'})),
             transition('void => *', [
-              style({
-                opacity: 0,
-                transform: 'translateX(-100%)'
-              }),
-              animate('0.2s ease-in')
+              animate(3000, keyframes([
+                style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+                style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+                style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+              ]))
             ]),
             transition('* => void', [
-              animate('0.2s 10 ease-out', style({
-                opacity: 0,
-                transform: 'translateX(100%)'
-              }))
+              animate(300, keyframes([
+                style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+                style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+                style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+              ]))
             ]),
         ])
     ],
