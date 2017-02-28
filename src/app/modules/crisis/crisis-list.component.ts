49854277d27a3dedca0340c4c6dc23e03c2e3b53
zhangaz1 <zhangaz1@hotmail.com>
2017-02-28 18:17:54 +0800

    高度动画

--- src/app/modules/crisis/crisis-list.component.ts
+++ src/app/modules/crisis/crisis-list.component.ts
@@ -14,23 +14,9 @@ import { CrisisService }     from './crisis.service';
     templateUrl: './crisis-list.component.html',
     animations: [
         trigger('crisisState', [
-            state('inactive', style({transform: 'translateX(0) scale(1)'})),
-            state('active',   style({transform: 'translateX(0) scale(1.1)'})),
-            transition('inactive => active', animate('100ms ease-in')),
-            transition('active => inactive', animate('100ms ease-out')),
-            transition('void => inactive', [
-              style({transform: 'translateX(-100%) scale(1)'}),
-              animate(100)
-            ]),
-            transition('inactive => void', [
-              animate(100, style({transform: 'translateX(100%) scale(1)'}))
-            ]),
-            transition('void => active', [
-              style({transform: 'translateX(0) scale(0)'}),
-              animate(200)
-            ]),
-            transition('active => void', [
-              animate(200, style({transform: 'translateX(0) scale(0)'}))
+            transition(':enter', [
+              style({height: 0}),
+              animate(250, style({height: '*'}))
             ]),
         ])
     ],
