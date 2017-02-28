ae266a59973339165f072870a93063183fcc52d4
zhangaz1 <zhangaz1@hotmail.com>
2017-02-28 19:52:51 +0800

    use hostingListener

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,11 +1,33 @@
-import { Directive, ElementRef }    from '@angular/core';
+import { Directive, ElementRef, HostListener, }    from '@angular/core';
 
 @Directive({
     selector: '[highlight]',
 })
 export class HighlightDirective {
-    constructor(el: ElementRef) {
-        el.nativeElement.style.backgroundColor = 'gold';
+    private oldColor: string;
+
+    constructor(private el: ElementRef) {
+        // el.nativeElement.style.backgroundColor = 'gold';
         console.log(`* AppRoot highlight called for ${el.nativeElement.tagName}`);
+    }
+
+    @HostListener('mouseenter')
+    onMouseEnter() {
+        this.oldColor = this.getBackgroundColor();
+        this.highlight('yellow');
+    }
+
+    @HostListener('mouseleave')
+    onMouseLeave() {
+        this.highlight(this.oldColor);
+    }
+
+
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+
+    private getBackgroundColor(): string {
+        return this.el.nativeElement.style.backgroundColor;
     }
 }
