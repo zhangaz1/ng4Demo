b1caed38ecb82c2e290ce2b3348ce4f926b9b0c9
zhangaz1 <zhangaz1@hotmail.com>
2017-03-01 11:20:09 +0800

    改回默认模拟样式

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -6,7 +6,7 @@ import { APP_CONFIG, AppConfig }     from './../app-config';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class AppComponent implements OnInit {  
   fontSizePx: number;
