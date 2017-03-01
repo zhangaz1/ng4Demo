3b6a71823709dab68b6d2bcb046753a0893abb5f
zhangaz1 <zhangaz1@hotmail.com>
2017-03-01 14:08:26 +0800

    codeformat

--- src/app/modules/toh/components/list/toh-list.component.ts
+++ src/app/modules/toh/components/list/toh-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     // selector: 'toh-list',
-    templateUrl: './toh-list.component.html'
+    templateUrl: './toh-list.component.html',
 })
 export class TohListComponent implements OnInit {
     constructor() { }
