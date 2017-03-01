45c14e7937a1f9a5f999845311dca138cbaebc3c
zhangaz1 <zhangaz1@hotmail.com>
2017-03-01 14:18:55 +0800

    add empty add hero

--- src/app/modules/toh/components/list/toh-list.component.ts
+++ src/app/modules/toh/components/list/toh-list.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero }     from './../../../../models/hero';
+
 @Component({
     moduleId: module.id,
     // selector: 'toh-list',
     templateUrl: './toh-list.component.html',
 })
 export class TohListComponent implements OnInit {
+    errorMessage: string;
+    heroes: Hero[];
+    mode = 'observable';
     constructor() { }
 
     ngOnInit() { }
+
+    addHero(heroName: string) {
+        if(!heroName) {
+            return;
+        }
+        this.errorMessage = 'no service';
+    }
 }
