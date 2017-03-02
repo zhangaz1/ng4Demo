ce4adeaa92a32ea2d280df0e62b85cfa8fea09e3
zhangaz1 <zhangaz1@hotmail.com>
2017-03-02 11:36:27 +0800

    增加虑抖和顺序

--- src/app/modules/toh/components/wiki/wiki.component.ts
+++ src/app/modules/toh/components/wiki/wiki.component.ts
@@ -1,5 +1,6 @@
-import { Component }     from '@angular/core';
+import { Component, OnInit }     from '@angular/core';
 
+import { Subject }     from 'rxjs/Subject';
 import { Observable }     from 'rxjs/Observable';
 
 import { WikiService }     from './../../services/wiki.service';
@@ -10,13 +11,24 @@ import { WikiService }     from './../../services/wiki.service';
     templateUrl: 'wiki.component.html',
     providers: [WikiService],
 })
-export class WikiComponent {
+export class WikiComponent implements OnInit {
     items: Observable<string[]>;
+
+    private searchTermStream = new Subject<string>();
 
     constructor(private wikiService: WikiService) { }
 
+    ngOnInit() {
+        let wikiService = this.wikiService;
+        let doSearch = wikiService.search.bind(wikiService);
+
+        this.items = this.searchTermStream
+                        .debounceTime(300)
+                        .distinctUntilChanged()
+                        .switchMap(doSearch);
+    }
+
     search(term: string) {
-        this.items = this.wikiService
-                            .search(term);
+        this.searchTermStream.next(term);
     }
 }
