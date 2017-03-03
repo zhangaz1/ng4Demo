d10e42f5cafdd389d9387203db86c6045618ac6d
zhangaz1 <zhangaz1@hotmail.com>
2017-03-03 09:16:39 +0800

    test unsubscrib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable }   from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() {
+    testUnsubscribe1();
+  }
+}
+
+function testUnsubscribe1() {
+    let observable = Observable.create(function subscribe(observer) {
+      let i = 0;
+
+      let intervalID = setInterval(() => {
+        observer.next(i++);
+      }, 1000);
+      
+      return function unsubscribe() {
+        clearInterval(intervalID);
+      };
+    });
+
+    let unsubscrib1 = observable.subscribe(x => console.log('sub1:', x));
+    let unsubscrib2;
+
+    setTimeout(() => {
+      unsubscrib2 = observable.subscribe(x => console.log('sub2:', x));
+    }, 3000);
+
+    setTimeout(() => unsubscrib2.unsubscribe(), 7000);
+    setTimeout(() => unsubscrib1.unsubscribe(), 10000);
 }
