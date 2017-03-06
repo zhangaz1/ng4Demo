6b12a8f91c36dccdc5c39261a3b2e1217037b8d0
zhangaz1 <zhangaz1@hotmail.com>
2017-03-06 08:28:14 +0800

    child subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,24 @@ export class AppComponent implements OnInit {
     console.clear();
     // testUnsubscribe1();
     // testObservable1();
-    testObservable2();    
-    // testObservable3();    
+    // testObservable2();    
+    // testObservable3();
+    testSubscribeAdd();
   }
+}
+
+function testSubscribeAdd() {
+  var observable1 = Observable.interval(400);
+  var observable2 = Observable.interval(300);
+
+  var subscription = observable1.subscribe(x => console.log('first:', x));
+  var childSubscription = observable2.subscribe(x => console.log('second:', x));
+
+  subscription.add(childSubscription);
+
+  setTimeout(() => {
+    subscription.unsubscribe();
+  }, 1000);
 }
 
 function testObservable3() {
