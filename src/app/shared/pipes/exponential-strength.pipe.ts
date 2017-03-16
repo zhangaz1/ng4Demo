c0af91d206dd19e3d9f938d74562a4a0aa511f46
zhngaz1 <zhangaz1@hotmail.com>
2017-03-16 20:49:17 +0800

    fix

--- src/app/shared/pipes/exponential-strength.pipe.ts
+++ src/app/shared/pipes/exponential-strength.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExponentialStrengthPipe implements PipeTransform {
     transform(value: number, exponent: string): number {
-        let exp  parseFloat(exponent);
+        let exp =  parseFloat(exponent);
         return Math.pow(value, isNaN(exp) ? 1 : exp);
     }
 }
