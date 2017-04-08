7d18433b37ea60a71f321e011a8cca8a1d730c34
zhangaz1 <zhangaz1@hotmail.com>
2017-04-08 19:25:19 +0800

    add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+import { PageNotFoundComponent } from './page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './component/app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    PageNotFoundComponent,
+    AppComponent,    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
