55f42f5ad970cc67fd90272f5a7964a96e7529b8
zhangaz1 <zhangaz1@hotmail.com>
2017-04-08 20:13:34 +0800

    add link sidekicks

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }     from '@angular/core';
+import { RouterModule }     from '@angular/router';
 
 import { HeroListComponent }     from './components/list/hero-list.component';
 
 @NgModule({
     declarations: [
         HeroListComponent,
+    ],
+    imports: [
+        RouterModule,
     ],
 })
 export class HeroModule { }
