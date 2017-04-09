b4f7fd1536645d1d477ebc906694e5baca03a1c6
zhangaz1 <zhangaz1@hotmail.com>
2017-04-09 16:32:12 +0800

    add  crisis home component

---
+++ src/app/modules/crisis/components/home/crisis-home.component.ts
@@ -0,0 +1,8 @@
+import { Component }     from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'crisis-home',
+    templateUrl: './crisis-home.component.html',
+})
+export class CrisisHomeComponent { }
--- src/app/modules/crisis/crisis-routing.module.ts
+++ src/app/modules/crisis/crisis-routing.module.ts
@@ -5,20 +5,27 @@ import {
 }     from '@angular/router';
 
 import { CrisisCenterComponent }     from './components/center/crisis-center.component';
+import { CrisisHomeComponent }       from './components/home/crisis-home.component';
 import { CrisisListComponent }       from './components/list/crisis-list.component';
 
 
 const appRoutes: Routes = [{
                             path: 'crisis-center',
                             component: CrisisCenterComponent,
-                        }, {
-                            path: 'crisis-list',
-                            component: CrisisListComponent,
-                        }];
+                            children: [{ 
+                                path: '',
+                                component: CrisisListComponent,
+                                children: [{
+                                    path: '',
+                                    component: CrisisHomeComponent,
+                                }, ],
+                            }, ],
+                        }, ];
 
 @NgModule({
     declarations: [
         CrisisCenterComponent,
+        CrisisHomeComponent,
         CrisisListComponent,
     ],
     imports: [       
