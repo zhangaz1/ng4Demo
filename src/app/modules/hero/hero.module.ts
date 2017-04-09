30a79ea483efa492e9f8815e216f17c91bf72d35
zhangaz1 <zhangaz1@hotmail.com>
2017-04-09 13:32:55 +0800

    add hero service provider

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }     from '@angular/core';
 
+import { HeroService }           from './services/hero.service';
 import { HeroRoutingModule }     from './hero-routing.module';
 
 @NgModule({
     imports: [
         HeroRoutingModule,
+    ],
+    providers: [
+        HeroService,
     ],
 })
 export class HeroModule { }
