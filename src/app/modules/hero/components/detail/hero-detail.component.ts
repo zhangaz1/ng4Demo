5713d7e4992d8a2c594409172a93ba05657bcff4
zhangaz1 <zhangaz1@hotmail.com>
2017-04-09 14:57:48 +0800

    add hero detail

---
+++ src/app/modules/hero/components/detail/hero-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component }     from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'hero-detail',
+    templateUrl: './hero-detail.component.html',
+})
+export class HeroDetailComponent { }
--- src/app/modules/hero/hero-routing.module.ts
+++ src/app/modules/hero/hero-routing.module.ts
@@ -4,15 +4,20 @@ import { Routes, RouterModule }     from '@angular/router';
 import { CoreModule }     from './../../core/core.module';
 
 import { HeroListComponent }     from './components/list/hero-list.component';
+import { HeroDetailComponent }     from './components/detail/hero-detail.component';
 
 const appRoutes: Routes = [{
                             path: 'hero-list',
                             component: HeroListComponent,
-                        }];
+                        }, {
+                            path: 'hero/:id',
+                            component: HeroDetailComponent,
+                        }, ];
 
 @NgModule({
     declarations: [
         HeroListComponent,
+        HeroDetailComponent,
     ],
     imports: [
         CoreModule,
