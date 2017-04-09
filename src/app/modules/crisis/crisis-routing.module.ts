af3aa639bb047777e2433a7fde8088fd07ef56bf
zhangaz1 <zhangaz1@hotmail.com>
2017-04-09 16:11:55 +0800

    add crisisCenter

--- src/app/modules/crisis/crisis-routing.module.ts
+++ src/app/modules/crisis/crisis-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule }                 from '@angular/core';
-import { Routes, RouterModule }     from '@angular/router';
+import {
+    Routes, 
+    RouterModule
+}     from '@angular/router';
 
-import { CrisisListComponent }     from './components/list/crisis-list.component';
+import { CrisisCenterComponent }     from './components/center/crisis-center.component';
+import { CrisisListComponent }       from './components/list/crisis-list.component';
 
 
 const appRoutes: Routes = [{
+                            path: 'crisis-center',
+                            component: CrisisCenterComponent,
+                        }, {
                             path: 'crisis-list',
                             component: CrisisListComponent,
                         }];
 
 @NgModule({
     declarations: [
+        CrisisCenterComponent,
         CrisisListComponent,
     ],
     imports: [       
