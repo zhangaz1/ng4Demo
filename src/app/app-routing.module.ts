c3bc56da9e1a1f06d988bfdbaae59450dbf0b0d9
zhangaz1 <zhangaz1@hotmail.com>
2017-04-20 19:57:23 +0800

    add compose-message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }                 from '@angular/core';
 import { Routes, RouterModule }     from '@angular/router';
 
+import { CoreModule }     from './core/core.module';
+
 import { PageNotFoundComponent } from './page-not-found.component';
 
 import { HeroModule }   from './modules/hero/hero.module';
 import { CrisisModule }     from './modules/crisis/crisis.module';
+
+import { ComposeMessageComponent }     from './component/popup/compose-message.component';
 
 import { HeroListComponent }     from './modules/hero/components/list/hero-list.component';
 import { CrisisListComponent }     from './modules/crisis/components/list/crisis-list.component';
@@ -17,13 +21,19 @@ const appRoutes: Routes = [{
                         }, {
                             path: '**',
                             component: PageNotFoundComponent,
-                        }];
+                        }, {
+                            path: 'compose',
+                            component: ComposeMessageComponent,
+                            outlet: 'popup',
+                        }, ];
 
 @NgModule({
     declarations: [
         PageNotFoundComponent,
+        ComposeMessageComponent,
     ],
     imports: [
+        CoreModule,        
         HeroModule,
         CrisisModule,
         RouterModule.forRoot(appRoutes),
