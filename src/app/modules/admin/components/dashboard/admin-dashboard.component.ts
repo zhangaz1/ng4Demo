91c03b4eee4f92b11e3fdc4984df15d1840c7545
zhngaz1 <zhangaz1@hotmail.com>
2017-05-08 21:48:40 +0800

    show preloaded modules in admin dashboard

--- src/app/modules/admin/components/dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/dashboard/admin-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit }     from '@angular/core';
 import { ActivatedRoute }       from '@angular/router';
 import { Observable }           from 'rxjs/Observable';
+import { SelectivePreloadingStrategyService }  from './../../../../common/selective-preloading-strategy.service';
 
 @Component({
     moduleId: module.id,
@@ -10,10 +11,14 @@ import { Observable }           from 'rxjs/Observable';
 export class AdminDashboardComponent { 
     sessionId: Observable<string>;
     token: Observable<string>;
+    modules: string[];
 
     constructor(
         private route: ActivatedRoute,
-    ) { }
+        private preloadStrategy: SelectivePreloadingStrategyService,
+    ) {
+        this.modules = preloadStrategy.preloadModules;
+    }
 
     ngOnInit() {
         this.sessionId = this.route
